236db9a48d23f8ee555db38ddc6658001a707f8e
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-05-11 12:43:52 -0700

    created javascript object for questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+function question (text,answers,correct){
+    this.text = text;
+    this.answers = answers;
+    this.correctAns = correct; //the index number of the correct answer in the answers array
+    this.correct;
+}
