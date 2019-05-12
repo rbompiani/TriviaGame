640522002ac7f27817d78bed2a7bbdd456c3829d
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-05-11 17:48:16 -0700

    added post-answer facts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-    function question (text,answers,correct){
+    function question (text,answers,correct, answerText){
         this.text = text;
         this.answers = answers;
         this.correctAns = correct; //the index number of the correct answer in the answers array
+        this.answerText = answerText;
         this.correct;
     }
 
@@ -11,16 +12,16 @@ $(document).ready(function(){
     var incorrectAnswers;
     var questions;
 
-    var q1 = new question("What was the name of the first Bond film?",["Dr. No", "Casino Royale", "On Her Majesty's Secret Service"], 0);
-    var q2 = new question("Which was the first Bond film to star Pierce Brosnan?", ["Tomorrow Never Dies", "Die Another Day", "Goldeneye"], 2);
-    var q3 = new question("Which Bond film is set primarily in Japan?", ["Octopussy", "You Only Live Twice", "Die Another Day"], 1);
-    var q4 = new question("SPECTRE stands for Special Executive For Counter-Intelligence, Terrorism, Revenge and... what?", ["Extinction","Extortion","Examination"], 1);
-    var q5 = new question("What does the “00” signify in 007?",["The rank of commander","A license to kill","A license to carry firearms"],1);
-    var q6 = new  question("Who sang the title theme for Diamonds Are Forever?",["Rita Coolidge", "A-Ha", "Shirley Bassey"], 2);
-    var q7 = new question("What famous landmark does Grace Jones jump from in A View to a Kill?", ["Big Ben", "The Eiffel Tower", "The Empire State Building"], 1);
-    var q8 = new question("Which of the following is NOT a James Bond Villain", ["Le Chiffre", "Erst Stavro Blofeld", "Richmond Valentine", "Auric Goldfinger"],2);
-    var q9 = new question("Which Bond film has recorded the biggest figure at the international box office?", ["Skyfall","Quantum of Solace", "Dr. No"], 0);
-    var q10 = new question("Which of the following actors did NOT play James Bond", ["Sean Connery", "Idris Elba", "Daniel Craig"], 1);
+    var q1 = new question("What was the name of the first Bond film?",["Dr. No", "Casino Royale", "On Her Majesty's Secret Service"], 0, "Dr. No, the first Bond film, was released in 1962");
+    var q2 = new question("Which was the first Bond film to star Pierce Brosnan?", ["Tomorrow Never Dies", "Die Another Day", "Goldeneye"], 2, "Pierce Brosnan made his first appearance as 007 in 1995's Goldeneye");
+    var q3 = new question("Which Bond film is set primarily in Japan?", ["Octopussy", "You Only Live Twice", "Die Another Day"], 1, "Most of the film You Only Live Twice was shot in Japan between 1966 & 1967");
+    var q4 = new question("SPECTRE stands for Special Executive For Counter-Intelligence, Terrorism, Revenge and... what?", ["Extinction","Extortion","Examination"], 1, "SPECTRE stands for Special Executive For Counter-Intelligence, Terrorism, Revenge, and Extortion");
+    var q5 = new question("What does the “00” signify in 007?",["The rank of commander","A license to kill","A license to carry firearms"],1, "The 00 represents a license to kill. The 7 means Bond was the 7th agent to receive this title");
+    var q6 = new  question("Who sang the title theme for Diamonds Are Forever?",["Rita Coolidge", "A-Ha", "Shirley Bassey"], 2, "Shirley Bassey sang the title song for 3 Bond films");
+    var q7 = new question("What famous landmark does Grace Jones jump from in A View to a Kill?", ["Big Ben", "The Eiffel Tower", "The Empire State Building"], 1, "Grace Jones lept from the Eiffel Tower in the 1985 Bond film");
+    var q8 = new question("Which of the following is NOT a James Bond Villain", ["Le Chiffre", "Erst Stavro Blofeld", "Richmond Valentine", "Auric Goldfinger"],2, "Richmond Valentine was the headlining villain from the Kingsmen francise");
+    var q9 = new question("Which Bond film has recorded the biggest figure at the international box office?", ["Skyfall","Quantum of Solace", "Dr. No"], 0, "2012 Skyfall netted 1.1 billon dollars at the box office");
+    var q10 = new question("Which of the following actors did NOT play James Bond", ["Sean Connery", "Idris Elba", "Daniel Craig"], 1, "Though rumored to be the next Bond, Idris Elba has never been awarded the role.");
 
     questions = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
 
@@ -43,6 +44,14 @@ $(document).ready(function(){
             if (answers == correctIndex){
                 newAnswer.addClass("correct");
             }
+
+            newAnswer.on("click", function(){
+                // if correct...
+                // replace question with correct
+
+                // else if incorrect...
+                // replace question with incorrect.
+            })
 
             //append answer to list
             $("#answers").append(newAnswer);
